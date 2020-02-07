0380ab2e1ee69fa6de4846854db4a7dac1eaef20
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-07 20:01:59 +0900

    initial

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'tweets#index'
+  namespace :tweets do
+    resources :searches, only: :index
+  end
+  resources :tweets do
+    resources :comments, only: :create
+#    collection do
+#      get 'search'
+#    end
+  end
+  resources :users, only: :show
+
+end
