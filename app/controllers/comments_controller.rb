44e68859b7ac96ede42a36a578ee9e7bf1c6025a
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-17 10:09:42 +0900

    jquery

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+  
   end
 
   private
